cd4b301062d532667e3a9bb6056d1d278a7c1465
LukmanAlHakim23as <lukmanhakim.23as2gmail.com>
2024-07-09 11:26:45 +0700

    Memperbarui Landing Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,120 +1,156 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Perpustakaan 40</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Perpustakaan 40</title>
 
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="AdminLTE/plugins/fontawesome-free/css/all.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="AdminLTE/dist/css/adminlte.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="AdminLTE/plugins/fontawesome-free/css/all.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="AdminLTE/dist/css/adminlte.min.css">
 
-  <style>
-    /* Custom CSS untuk perpustakaan */
+    <style>
+        /* Custom CSS untuk perpustakaan */
 
-    .navbar-brand .brand-image {
-      width: 40px; /* Ukuran logo */
-    }
+        .navbar-brand .brand-image {
+            width: 40px;
+            /* Ukuran logo */
+        }
 
-    .navbar-brand .brand-text {
-      font-size: 1.2rem; /* Ukuran teks brand */
-    }
+        .navbar-brand .brand-text {
+            font-size: 1.2rem;
+            /* Ukuran teks brand */
+        }
 
-    .content-wrapper {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      height: 65vh;
-      background-color: #f8f9fa; /* Warna latar belakang konten */
-    }
+        .hero-section {
+            margin-top: 100px;
+            display: flex;
+            margin-left: 50px;
+            margin-right: 100px;
+            justify-content: space-between;
+            align-items: center;
+        }
 
-    .landing-content {
-      text-align: center;
-    }
+        .hero-section .left {
+            padding: 10px 80px;
 
-    .btn {
-      padding: 10px 20px;
-      background-color: #007bff;
-      color: #fff;
-      text-decoration: none;
-      border-radius: 5px;
-      font-size: 1em;
-      transition: background-color 0.3s ease;
-    }
+        }
 
-    .btn:hover {
-      background-color: #0056b3;
-    }
-  </style>
+        .hero-section .left h4 {
+            margin-bottom: 10px;
+        }
+
+        .hero-section .left h2 {
+            margin-bottom: 15px;
+        }
+
+        .hero-section h3 {
+            margin-bottom: 20px;
+            font-size: 2rem;
+        }
+
+        .hero-section h2 {
+            font-size: 3rem;
+        }
+
+        .hero-section p {
+            font-size: 1rem;
+            margin-bottom: 30px;
+            font-weight: 500;
+            text-align: justify;
+        }
+
+        .btn {
+            display: inline-block;
+            padding: 0.5rem 1.5rem;
+            background: black;
+            border-radius: 4rem;
+            font-size: 1rem;
+            font-weight: 600;
+            color: #ffffff;
+            margin-top: 1rem;
+        }
+
+        .hero-section .right img {
+            width: 35vw;
+            margin-bottom: 100px;
+        }
+    </style>
 </head>
+
 <body class="hold-transition layout-top-nav">
-<div class="wrapper">
+    <div class="wrapper">
 
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
-      <div class="container">
-        <a href="../../index3.html" class="navbar-brand">
-          <img src="image/logo40.png" alt="Logo SMKN 40" class="brand-image img-circle elevation-3" style="opacity: .8">
-          <span class="brand-text font-weight-light">Perpustakaan 40</span>
-        </a>
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
+            <div class="container">
+                <a href="../../index3.html" class="navbar-brand">
+                    <img src="image/logo40.png" alt="Logo SMKN 40" class="brand-image img-circle elevation-3"
+                        style="opacity: .8">
+                    <span class="brand-text font-weight-light">Perpustakaan 40</span>
+                </a>
 
-        <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        
-      </div>
-    </nav>
-    <!-- /.navbar -->
-  <!-- /.navbar -->
+                <button class="navbar-toggler order-1" type="button" data-toggle="collapse"
+                    data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Main content -->
-    <div class="content">
-      <div class="container">
-        <div class="landing-content">
-          <h1>Selamat Datang di Perpustakaan 40</h1>
-          <p class="subheading">Perpustakaan 40 adalah tempat yang sempurna untuk menjelajahi dunia pengetahuan dan petualangan. Di sini, Anda akan menemukan berbagai macam buku yang menginspirasi, mendidik, dan menghibur. Mari bergabung bersama kami dan temukan dunia yang tak terbatas melalui halaman-halaman buku.</p>
-          <a href="/login" class="btn">Get Started</a>
+            </div>
+        </nav>
+        <!-- /.navbar -->
+        <!-- /.navbar -->
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="hero-section">
+            <div class="left">
+                <h2>Pepustakaan 40 Jakarta</h2>
+                <p>Perpustakaan 40 adalah tempat yang sempurna untuk menjelajahi dunia pengetahuan dan petualangan. Di
+                    sini, Anda akan menemukan berbagai macam buku yang menginspirasi, mendidik, dan menghibur. Mari
+                    bergabung bersama kami dan temukan dunia yang tak terbatas melalui halaman-halaman buku.</p>
+                <a href="/login" class="btn">Login Sekarang</a>
+            </div>
+            <div class="right">
+                <img src="image/Notebook.png" alt="">
+            </div>
         </div>
-      </div>
+        <!-- /.content-wrapper -->
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+
+        <!-- Main Footer -->
+        <footer class="main-footer">
+            <div class="container">
+                <div class="float-left">
+                    <strong>Perpustakaan 40 &copy; 2024</strong>
+                </div>
+                <div class="float-right">
+                    <strong>Contact Us: </strong> support@perpustakaan40.com
+                </div>
+            </div>
+        </footer>
     </div>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+    <!-- ./wrapper -->
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
+    <!-- REQUIRED SCRIPTS -->
 
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <div class="container">
-      <div class="float-left">
-        <strong>Perpustakaan 40 &copy; 2024</strong>
-      </div>
-      <div class="float-right">
-        <strong>Contact Us: </strong> support@perpustakaan40.com
-      </div>
-    </div>
-  </footer>
-</div>
-<!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-
-<!-- jQuery -->
-<script src="AdminLTE/plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="AdminLTE/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="AdminLTE/dist/js/demo.js"></script>
+    <!-- jQuery -->
+    <script src="AdminLTE/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="AdminLTE/dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="AdminLTE/dist/js/demo.js"></script>
 </body>
+
 </html>
